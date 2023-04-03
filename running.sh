16f4e3791997057043810958c299ecf8fe709b3a
oleksandr_kochubei <kochubey.alexander94@gmail.com>
2023-04-03 23:15:58 +0300

    [fixed] path error in running.sh file

--- running.sh
+++ running.sh
@@ -4,10 +4,10 @@ set -e  # Exit immediately if a command exits with a non-zero status code
 
 chmod +x scripts/shared-scripts/*.sh
 
-./scripts/shares-scripts/is-docker-installed.sh
+./scripts/shared-scripts/is-docker-installed.sh
 
-./scripts/shares-scripts/is-app-directories-exists.sh
+./scripts/shared-scripts/is-app-directories-exists.sh
 
-./scripts/shares-scripts/prepare-docker-env-file.sh
+./scripts/shared-scripts/prepare-docker-env-file.sh
 
 docker-compose up --build
