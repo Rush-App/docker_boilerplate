040f1666a48bf7495a741e728d194d39d0c4494e
oleksandr_kochubei <kochubey.alexander94@gmail.com>
2023-04-04 12:40:58 +0300

    [added] cache-clear.sh (for backend) inside install.sh

--- running.sh
+++ running.sh
@@ -2,12 +2,14 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status code
 
-chmod +x scripts/shared-scripts/*.sh
+chmod +x scripts/shared-scripts/*.sh scripts/backend-app/*.sh
 
 ./scripts/shared-scripts/is-docker-installed.sh
 
 ./scripts/shared-scripts/is-app-directories-exists.sh
 
 ./scripts/shared-scripts/prepare-docker-env-file.sh
+
+./scripts/backend-app/cache-clear.sh
 
 docker-compose up --build
