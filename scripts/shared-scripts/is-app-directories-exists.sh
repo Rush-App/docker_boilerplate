835c453824f60dbcd7a59969b7bf71cd5eba221a
oleksandr_kochubei <kochubey.alexander94@gmail.com>
2023-04-04 12:30:21 +0300

    [updated] local DB data [fixed] path to apps directories

--- scripts/shared-scripts/is-app-directories-exists.sh
+++ scripts/shared-scripts/is-app-directories-exists.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "$SCRIPT_DIR/constants.sh"
 
 # Check if frontend-app directory exists
-if [ ! -d "$DOCKER_ROOT_DIRECTORY../frontend-app" ]; then
+if [ ! -d "$FRONTEND_APP_DIRECTORY" ]; then
     echo "Error: frontend-app directory does not exist."
     exit 1
 fi
 
 # Check if backend-app directory exists
-if [ ! -d "$DOCKER_ROOT_DIRECTORY../backend-app" ]; then
+if [ ! -d "$BACKEND_APP_DIRECTORY" ]; then
     echo "Error: backend-app directory does not exist."
     exit 1
 fi
